72c94087e57a8ff59799057230e3e98964d07313
ilushka-obnimashka <i.trushkin@g.nsu.ru>
2025-03-31 05:17:43 +0700

    feat : implemented function for extract audio from video

---
+++ Source/video2audio.py
@@ -0,0 +1,17 @@
+from moviepy import VideoFileClip
+
+from Source.audio2text_timestamps import audio_format_transcoder
+from Source.temp_file_manager import TempFilesManager
+
+
+def video2audio(video_path: str) -> str:
+    """
+    It extracts an audio track from a video file, converts it into a mono and sets the sampling frequency of 16khz, then
+    saves in format .Wav
+    """
+
+    video_clip = VideoFileClip(video_path)
+    audio_path = TempFilesManager().create_temp_file("temp.wav")
+    video_clip.audio.write_audiofile(audio_path)
+
+    return audio_format_transcoder(audio_path)
