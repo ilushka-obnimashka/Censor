1d349c185c529dfda27f69d67885f22580809e79
ilushka-obnimashka <i.trushkin@g.nsu.ru>
2025-05-13 00:08:29 +0700

    feat : API_URL = os.getenv("API_URL")

--- app.py
+++ app.py
@@ -1,31 +1,14 @@
 import mimetypes
+import os
 import traceback
 
 import gradio as gr
 import requests
 
-from utils.minio_manager import minio_client
+from utils import minio_client
+from main_config import CATEGORIES
 
-API_URL = "http://localhost:8000/process/"
-
-CATEGORIES = {
-    "Сигареты": "cigarette",
-    "18+": {
-        "Обнажённые ягодицы": "BUTTOCKS_EXPOSED",
-        "Обнажённая женская грудь": "FEMALE_BREAST_EXPOSED",
-        "Обнажённые женские гениталии": "FEMALE_GENITALIA_EXPOSED",
-        "Обнажённая мужская грудь": "MALE_BREAST_EXPOSED",
-        "Обнажённый анус": "ANUS_EXPOSED",
-        "Обнажённые подмышки": "ARMPITS_EXPOSED",
-        "Обнажённый живот": "BELLY_EXPOSED",
-        "Обнажённые мужские гениталии": "MALE_GENITALIA_EXPOSED",
-    },
-    "Экстремизм": {
-        "Символика ЛГБТ": "lgbt",
-        "Свастика": "svastika",
-    },
-    "Нецензурная лексика": "bad_words"
-}
+API_URL = os.getenv("API_URL")
 
 
 def get_selected_categories():
