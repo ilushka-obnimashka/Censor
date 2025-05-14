ab9a7458836dbfc0a74d352d0b66a8416b39bc5e
ilushka-obnimashka <i.trushkin@g.nsu.ru>
2025-05-15 04:54:08 +0700

    fix (NudeDetector): Детектор запускает модель распределенно на providers=["CUDAExecutionProvider", 'CPUExecutionProvider'].

--- backend/plugins_system/detectors/nude_detector.py
+++ backend/plugins_system/detectors/nude_detector.py
@@ -1,12 +1,31 @@
+import os
+
+import onnxruntime
 from nudenet import NudeDetector as OrigNudeDetector
 
 from .base_detector import *
 
 
+class CustomNudeDetector(OrigNudeDetector):
+    def __init__(self, model_path=None, providers=None, inference_resolution=320):
+        onnxruntime.preload_dlls(cuda=True, cudnn=True)
+        self.onnx_session = onnxruntime.InferenceSession(
+            os.path.join(os.path.dirname(__file__), "320n.onnx")
+            if not model_path
+            else model_path,
+            providers=["CUDAExecutionProvider", 'CPUExecutionProvider'],
+        )
+        model_inputs = self.onnx_session.get_inputs()
+
+        self.input_width = inference_resolution
+        self.input_height = inference_resolution
+        self.input_name = model_inputs[0].name
+
+
 class NudeDetector(BaseDetector):
     def __init__(self, device: str = 'cuda'):
         super().__init__('backend/models/nudenet640m.onnx', device)
-        self.model = OrigNudeDetector(model_path=self.model_path, inference_resolution=IMAGE_SIZE)
+        self.model = CustomNudeDetector(model_path=self.model_path, inference_resolution=IMAGE_SIZE)
 
     def detect(self, img: Any) -> List[Dict[str, Any]]:
         """
